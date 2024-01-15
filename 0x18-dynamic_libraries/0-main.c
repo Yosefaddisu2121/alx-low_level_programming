cd8db86f3721466313bed447fca193d2e28b531f
Yosefaddisu2121 <yosefaddisu777@gmail.com>
2024-01-15 09:51:05 -0800

    trail commit2

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h> // Include the standard library header for EXIT_SUCCESS
+
+int _strlen(const char *str); // Declaration of _strlen
+
+int main(void) {
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return EXIT_SUCCESS; // Use EXIT_SUCCESS instead of just 'EXIT_SUCCESS'
+}
