1e615af0e651068946d8c6708ace3328caf3180e
Yosefaddisu2121 <yosefaddisu777@gmail.com>
2024-05-06 23:48:28 -0700

    qwert

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include "search_algos.h"
 
 /**
@@ -9,24 +10,22 @@
  *  REturn: If the value is not peresent or the array is NULL, -1. otherwise,the first index where the value is located
  *
  */
-int linear_search(int *array,size_t size,int value)
+int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
 	/*check if the array is NULL */
 	if (array == NULL)
 		return (-1);
-        /*Iterate over each element of the array */
-	for (i = 0; i < size;i++)
-	{ 
-		/*Print the value being checked*/
-		printf("Value checked array[%ld] - [%d]\n",i,array[i]);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] - [%d]\n", i, array[i]);
 
 		/* if the value is found */
 		if (array[i] == value)
 			/*Return the index of the value */
 			return (i);
 	}
-	/* Value not found, return -1 */ 
+	/* Value not found, return -1 */
 	return (-1);
 }
